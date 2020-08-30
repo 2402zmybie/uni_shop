9488a1f78edf2c71e440e48be5d95410b38e4963
2402zmybie <2402368990@qq.com>
2020-08-30 14:29:06 +0800

    首页网格列表完成

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -30,16 +30,17 @@
 			<view class="title">推荐商品</view>
 			<!-- list -->
 			<view class="goods_list">
-				<view class="goods_item">
-					<image class="goods_item_image" src="https://cdn.pixabay.com/photo/2014/04/14/20/11/japanese-cherry-trees-324175_150.jpg"></image>
+				<view class="goods_item" v-for="(item,index) in goods" :key="item.id">
+					<image class="goods_item_image" :src="item.previewURL"></image>
 					<view class="price">
-						<text>869874</text>
-						<text>2615</text>
+						<text>${{ item.downloads }}</text>   
+						<text>${{ item.favorites }}</text>
 					</view>
 					<view class="name">
-						Hans
+						{{ item.user }}
 					</view>
 				</view>
+				
 			</view>
 		</view>
 	</view>
@@ -53,21 +54,17 @@
 				indicatorDots: true,
 				autoplay: true,
 				interval: 2000,
-				duration: 500
+				duration: 500,
+				goods:[],
 			}
 		},
 		//请求数据
 		onLoad() {
 			this.getSwipers()
+			this.getHotPictures()
 		},
 		methods: {
-			// async getSwipers() {
-			// 	const res = await this.$myRequest({
-			// 		url:'/banner/json'
-			// 	})
-			// 	console.log(res)
-			// }
-
+			//请求轮播图
 			getSwipers() {
 				uni.request({
 					url: 'https://www.wanandroid.com/banner/json',
@@ -83,6 +80,17 @@
 					},
 					fail: (e) => {
 						console.log(e)
+					}
+				})
+			},
+			//请求网络图片
+			getHotPictures() {
+				uni.request({
+					url:'https://pixabay.com/api/?key=17921301-974ad23d82135fa91669f2b9f&q=cats&image_type=photo&page=1',
+					success: (res) => {
+						// console.log(res)
+						this.goods = res.data.hits
+						console.log(this.goods.length)
 					}
 				})
 			}
@@ -139,15 +147,39 @@
 			}
 			.goods_list {
 				padding: 0 15rpx;
-				// 条目
+				display: flex;
+				flex-wrap: wrap;
+				justify-content: space-between;
+				// 条目, 给盒子设置padding之后盒子会被撑大,要想不被撑大,则设置box-sizing: border-box;
 				.goods_item {
 					background: white;
 					width: 355rpx;
+					margin: 10rpx 0;
+					padding: 15rpx;
+					box-sizing: border-box;
 					// 条目图片
 					.goods_item_image {
-						
 						width: 284rpx;
 						height: 150px;
+						display: block;
+						margin: 0 auto;
+					}
+					.price{
+						color: $shop-color;
+						font-size: 36rpx;
+						margin: 20rpx 0 5rpx 0;
+						text:nth-child(2) {
+							color: #ccc;
+							font-size: 28rpx;
+							margin-left: 10rpx;
+							text-decoration: line-through;
+						}
+					}
+					.name {
+						font-size: 28rpx;
+						line-height: 50rpx;
+						padding-bottom: 15rpx;
+						padding-top: 10rpx;
 					}
 				}
 			}
